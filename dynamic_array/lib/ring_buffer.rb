b05e23bc09f84bfff3fb5e41760f903844b1451b
Brandt <brandtsheets@gmail.com>
2017-12-21 18:05:50 -0800

    Implement ring buffer

--- dynamic_array/lib/ring_buffer.rb
+++ dynamic_array/lib/ring_buffer.rb
@@ -4,30 +4,56 @@ class RingBuffer
   attr_reader :length
 
   def initialize
+    @length = 0
+    @capacity = 8
+    @store = StaticArray.new(@capacity)
+    @start_idx = 0
   end
 
   # O(1)
   def [](index)
+    check_index(index)
+    @store[(@start_idx + index) % @capacity]
   end
 
   # O(1)
   def []=(index, val)
+    check_index(index)
+    @store[(@start_idx + index) % @capacity] = val
   end
 
   # O(1)
   def pop
+    raise("index out of bounds") if @length == 0
+    result = @store[(@start_idx + @length  - 1) % @capacity]
+    @store[(@start_idx + @length  - 1) % @capacity] = nil
+    @length -= 1
+    result
   end
 
   # O(1) ammortized
   def push(val)
+    resize! if @length == @capacity
+    @store[(@start_idx + @length) % @capacity] = val
+    @length += 1
   end
 
   # O(1)
   def shift
+    raise("index out of bounds") if @length == 0
+    result = @store[@start_idx]
+    @store[@start_idx] = nil
+    @start_idx = (@start_idx + 1) % capacity
+    @length -= 1
+    result
   end
 
   # O(1) ammortized
   def unshift(val)
+    resize! if @length == @capacity
+    @start_idx = (@start_idx - 1) % @capacity
+    @store[@start_idx] = val
+    @length += 1
   end
 
   protected
@@ -35,8 +61,17 @@ class RingBuffer
   attr_writer :length
 
   def check_index(index)
+    raise("index out of bounds") if index < 0 || index >= @length
   end
 
   def resize!
+    @capacity *= 2
+    new_store = StaticArray.new(@capacity)
+    @length.times do |i|
+      index = (@start_idx + i) % @length
+      new_store[i] = @store[index]
+    end
+    @store = new_store
+    @start_idx = 0
   end
 end
