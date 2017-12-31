32d31173a9c69ea6519a8e6997a8f63d8aa105aa
Brandt <brandtsheets@gmail.com>
2017-12-30 22:40:34 -0800

    Solve palindrome exercise

--- hashmap/lib/p07_practical.rb
+++ hashmap/lib/p07_practical.rb
@@ -1,5 +1,17 @@
 require_relative 'p05_hash_map'
 
 def can_string_be_palindrome?(string) 
-  
+    map = HashMap.new  
+    string.each_char do |char|
+        if map[char]
+            map[char] += 1
+        else
+            map[char] = 1
+        end 
+    end
+    num_odds = 0
+    map.each do |key, val|
+        num_odds += 1 if val.odd?
+    end 
+    num_odds < 2
 end
