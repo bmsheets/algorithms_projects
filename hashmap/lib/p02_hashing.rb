23ba9a8208e958ab598314e7a8494c162da7daf3
Brandt <brandtsheets@gmail.com>
2017-12-30 17:29:41 -0800

    Write hashing functions

--- hashmap/lib/p02_hashing.rb
+++ hashmap/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    total = 0
+    self.each_with_index do |el, idx|
+        if el.is_a?(String)
+            token = el.ord
+        else
+            token = el.to_i
+        end
+        total += token * (idx + 1)
+    end
+    total.hash
   end
 end
 
 class String
   def hash
+    self.chars.hash
   end
 end
 
@@ -16,6 +27,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.flatten.map {|el| el.to_s}.sort.hash
   end
 end
