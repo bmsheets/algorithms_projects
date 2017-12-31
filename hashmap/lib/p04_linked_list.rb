c0396fb3cd64f438e80a696484651453953f12e9
Brandt <brandtsheets@gmail.com>
2017-12-30 18:20:19 -0800

    Implement basic linked list methods

--- hashmap/lib/p04_linked_list.rb
+++ hashmap/lib/p04_linked_list.rb
@@ -13,13 +13,20 @@ class Node
   end
 
   def remove
+    @prev.next = @next if @prev
+    @next.prev = @prev if @next
     # optional but useful, connects previous node to next node
     # and removes self from list.
   end
 end
 
 class LinkedList
+  attr_reader :length
+
   def initialize
+    @head = nil
+    @tail = nil
+    @length = 0
   end
 
   def [](i)
@@ -28,27 +35,74 @@ class LinkedList
   end
 
   def first
+    @head
   end
 
   def last
+    @tail
   end
 
   def empty?
+    @length == 0
   end
 
   def get(key)
+    node = @head
+    while (node)
+        return node.val if node.key == key
+        node = node.next
+    end
+    nil
   end
 
   def include?(key)
+    node = @head
+    while (node)
+        return true if node.key == key
+        node = node.next
+    end 
+    false
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    if empty?
+        @head = node
+        @tail = node
+    else
+        node.prev = @tail
+        @tail.next = node
+        @tail = node
+    end
+    @length += 1
   end
 
   def update(key, val)
+    node = @head
+    while(node)
+        node.val = val if node.key == key
+        node = node.next
+    end
   end
 
   def remove(key)
+    node = @head
+    while(node)
+        if node.key == key 
+            if node == @head
+                @head.next.prev = nil
+                @head = @head.next
+            elsif node == @tail
+                @tail.prev.next = nil
+                @tail = @tail.prev
+            else
+                node.prev.next = node.next
+                node.next.prev = node.prev
+            end
+            @length -= 1
+        end
+        node = node.next
+    end
   end
 
   def each
