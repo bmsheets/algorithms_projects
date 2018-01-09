1324cd0da1bacb7dc7ec17e5b2ee016bcbc68ee0
Brandt <brandtsheets@gmail.com>
2018-01-09 11:06:33 -0800

    Add tarjan's topological sort algorithm

--- graphs/lib/topological_sort.rb
+++ graphs/lib/topological_sort.rb
@@ -20,5 +20,22 @@ end
 
 # Tarian's
 def topological_sort2(vertices)
+  result = []
+  nodes = vertices.map {|vertex| [vertex, 0]}
+  unmarked = nodes.select {|node| node[1] == 0}
+  while unmarked.length > 0
+    visit(unmarked.first, result)
+    unmarked = nodes.select {|node| node[1] == 0}
+  end
+end
 
+def visit(node, arr)
+  return if node[1] == 2
+  return if node[1] == 1
+  node[0].out_edges.each do |edge|
+    visit(edge.to_vertex, arr)
+  end
+  node[1] = 1
+  node[1] = 2
+  result.unshift(node[0])
 end
