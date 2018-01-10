8b92ec71f98f030784c84680d6ac01091d968fe4
Brandt <brandtsheets@gmail.com>
2018-01-10 00:03:31 -0800

    Solve frog hops problem

--- dynamic_programming/lib/dynamic_programming.rb
+++ dynamic_programming/lib/dynamic_programming.rb
@@ -2,6 +2,11 @@ class DynamicProgramming
 
   def initialize
     @blair_cache = {1 => 1, 2 => 2}
+    @frog_cache = {
+      1 => [[1]],
+      2 => [[1, 1], [2]],
+      3 => [[1, 1, 1], [1, 2], [2, 1], [3]]
+    }
   end
 
   def blair_nums(n)
@@ -14,19 +19,57 @@ class DynamicProgramming
   end
 
   def frog_hops_bottom_up(n)
-
+    cache = frog_cache_builder(n)
+    cache[n]
   end
 
   def frog_cache_builder(n)
+    cache = {
+      1 => [[1]],
+      2 => [[1, 1], [2]],
+      3 => [[1, 1, 1], [1, 2], [2, 1], [3]]
+    }
+    return cache if n < 4
+    (4..n).each do |i|
+      result = []
+      prev1 = cache[i - 1]
+      prev2 = cache[i - 2]
+      prev3 = cache[i - 3]
 
+      [prev1, prev2, prev3].each_with_index do |prev, idx|
+        prev.each do |pattern1|
+          cache[idx + 1].each do |pattern2|
+            result.push(pattern1 + pattern2)
+          end
+        end
+      end
+
+      cache[i] = result.uniq
+    end
+    cache
   end
 
   def frog_hops_top_down(n)
-
+    frog_hops_top_down_helper(n)
   end
 
   def frog_hops_top_down_helper(n)
+    return @frog_cache[n] if n < 4
+    prev1 = frog_hops_top_down_helper(n - 1)
+    prev2 = frog_hops_top_down_helper(n - 2)
+    prev3 = frog_hops_top_down_helper(n - 3)
 
+    result = []
+    [prev1, prev2, prev3].each_with_index do |prev, idx|
+      prev.each do |pattern|
+        @frog_cache[idx + 1].each do |pattern2|
+          result.push(pattern + pattern2)
+        end
+      end
+    end
+
+    @frog_cache[n] = result.uniq
+    @frog_cache[n]
   end
 
   def super_frog_hops(n, k)
